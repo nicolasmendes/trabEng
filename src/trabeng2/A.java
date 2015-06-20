ba61426d86e978753c87ada38fa4a868ca2e1354
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 14:03:42 -0300

    versao 1.0

---
+++ src/trabeng2/A.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabeng2;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class A {
+
+private int A1;
+private float A2;
+
+ public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
