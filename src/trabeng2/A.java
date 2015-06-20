de4413f43b3912acc88d923460c742fa182b0327
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 15:07:52 -0300

    versao 1.2

--- src/trabeng2/A.java
+++ src/trabeng2/A.java
@@ -11,8 +11,18 @@ package trabeng2;
  */
 public class A {
 
-private int A1;
-private float A2;
+    private int A1;
+    private float A2;
+
+    private String Aramo4;
+
+    public String getAramo4() {
+        return Aramo4;
+    }
+
+    public void setAramo4(String Aramo4) {
+        this.Aramo4 = Aramo4;
+    }
 
     private String A3;
 
@@ -24,14 +34,14 @@ private float A2;
         this.A3 = A3;
     }
 
-
- public int getA1() {
+    public int getA1() {
         return A1;
     }
 
     public void setA1(int A1) {
         this.A1 = A1;
     }
+
     public float getA2() {
         return A2;
     }
@@ -39,19 +49,14 @@ private float A2;
     public void setA2(float A2) {
         this.A2 = A2;
     }
-    
-   public static float somar( int a, float b ) {      
-      return a + b;      
-   }    
-  
-   public static void main(String args[]) {  
-       int A1 =2;
-       float A2=3;
-        float result = somar(A1, A2);    
-   }    
-}  
-    
 
- 
-    
+    public static float somar(int a, float b) {
+        return a + b;
+    }
 
+    public void main(String args[]) {
+        A1 = 2;
+        A2 = 3;
+        float result = somar(A1, A2);
+    }
+}
