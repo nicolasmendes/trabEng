aea88df8fc95fd65eb8fdad715de4a6788452a9c
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 13:35:49 -0300

    novo commit

---
+++ src/trabeng/A.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabeng;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class A {
+
+
+private int A1;
+private float A2;
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+
+    
+}
