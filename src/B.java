57fe158694a741d13fd0b1088c651e23a2948bed
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 15:16:34 -0300

    versao 1.5

--- src/B.java
+++ src/B.java
@@ -10,4 +10,34 @@
  */
 public class B {
     
+    private int B1;
+    private float B2;
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+    
+        private String B4;
+
+    public String getB4() {
+        return B4;
+    }
+
+    public void setB4(String B4) {
+        this.B4 = B4;
+    }
+
+
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
 }
